8fedc899d97df1b8de263a2b04d282870f0b83f5
Gogang <yoonmin1130@gmail.com>
2017-11-12 17:59:06 +0900

    Test File before start project

--- MARVEL/MARVEL/ViewController.swift
+++ MARVEL/MARVEL/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Label_Change(_ sender: Any) {
+        Label.text = "Hello MARVEL!!"
+    }
+    
 }
 
